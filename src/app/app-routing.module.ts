4d7782ea7a0d2dd08aac820232b4bd0ab84a57d4
insafabdi <angelk6@outlook.fr>
2022-08-15 15:13:35 +0100

    "home page"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { DialogExpComponent } from './dialog-exp/dialog-exp.component';
-import { ListPatientComponent } from './list-patient/list-patient.component';
+import { HomeComponent } from './home/home.component';
 import { ListedesdocteursComponent } from './listedesdocteurs/listedesdocteurs.component';
 import { ListedesrendezVousComponent } from './listedesrendez-vous/listedesrendez-vous.component';
 import { LoginComponent } from './login/login.component';
 import { PrisedesrendezVousComponent } from './prisedesrendez-vous/prisedesrendez-vous.component';
 import { RegisterComponent } from './register/register.component';
-import { SpecialitesComponent } from './specialites/specialites.component';
+
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'listedesdocteurs',component:ListedesdocteursComponent},
   {path:'prisedesrendez-vous',component:PrisedesrendezVousComponent},
   {path:'listedesrendez-vous',component:ListedesrendezVousComponent},
-  {path:'patientsList',component:ListPatientComponent},
   {path:'dialog-exp',component:DialogExpComponent},
-  {path:'specialites',component:SpecialitesComponent}
+  {path:'home',component:HomeComponent},
+  {path:'contact',component:ContactComponent}
 
 ];
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,14 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  public Catégorie :string;
- public medecin : string;
- public patient : string;
-  showPassword=true;
+
   constructor() { }
-  visibility(){
-    this.showPassword=!this.showPassword;
-  }
 
   ngOnInit(): void {
   }
@@ -24,22 +18,5 @@ export class RegisterComponent implements OnInit {
   b(b:any){
     
   }
-  
-  // password: string = '';
-  // c_password: string = '';
-  // toggle1: boolean = false;
-  // toggle2: boolean = false;
-  // changeType(input_field_password, num){
-  //   if(input_field_password.type=="password")
-  //     input_field_password.type = "text";
-  //   else
-  //     input_field_password.type = "password";
-
-  //   if(num == 1)
-  //     this.toggle1 = !this.toggle1;
-  //   else
-  //     this.toggle2 = !this.toggle2;
-  // }
-
 
 }
