735fd36ef6d6a026a77ee05c7d09d77b72137d82
bennaceurnourhene <bennaceurnourhene@gmail.com>
2022-08-15 14:30:21 +0100

    commit nourhen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
 import { DialogExpComponent } from './dialog-exp/dialog-exp.component';
-import { HomeComponent } from './home/home.component';
+import { ListPatientComponent } from './list-patient/list-patient.component';
 import { ListedesdocteursComponent } from './listedesdocteurs/listedesdocteurs.component';
 import { ListedesrendezVousComponent } from './listedesrendez-vous/listedesrendez-vous.component';
 import { LoginComponent } from './login/login.component';
 import { PrisedesrendezVousComponent } from './prisedesrendez-vous/prisedesrendez-vous.component';
 import { RegisterComponent } from './register/register.component';
-
+import { SpecialitesComponent } from './specialites/specialites.component';
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'listedesdocteurs',component:ListedesdocteursComponent},
   {path:'prisedesrendez-vous',component:PrisedesrendezVousComponent},
   {path:'listedesrendez-vous',component:ListedesrendezVousComponent},
+  {path:'patientsList',component:ListPatientComponent},
   {path:'dialog-exp',component:DialogExpComponent},
-  {path:'home',component:HomeComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'specialites',component:SpecialitesComponent}
 
 ];
 
